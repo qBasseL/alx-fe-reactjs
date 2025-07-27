83bb817b2ae790af9a2d71c2fda53b651c48119d
Bassel <basselalaa17@gmail.com>
2025-07-27 17:01:08 +0300

    Done

---
+++ alx-react-app-new/src/components/MainContent.jsx
@@ -0,0 +1,12 @@
+function MainContent() {
+  return (
+    <main style={{ padding: '20px', backgroundColor: '#e8f0fe', minHeight: '200px' }}>
+      <p style={{ fontSize: '18px', color: '#333' }}>
+        Welcome to the city explorer! Here you can find interesting information about your favorite cities.
+      </p>
+    </main>
+  );
+}
+
+export default MainContent;
+
