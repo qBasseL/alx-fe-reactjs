afbaa3d064f243276b0d377a5315d0d4e9f14888
Bassel <basselalaa17@gmail.com>
2025-07-27 17:29:35 +0300

    done

--- my-company/src/App.jsx
+++ my-company/src/App.jsx
@@ -2,7 +2,7 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Home from './pages/Home';
 import About from './pages/About';
 import Services from './pages/Services';
-import Contact from './pages/Contact';
+import Contact from './components/Contact';
 import Navbar from './components/Navbar';
 
 function App() {
